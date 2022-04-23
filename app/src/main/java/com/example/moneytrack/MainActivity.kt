5d237c4f6059127b5ae82fef1c5b71e57b3537fc
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-04-23 15:08:05 +0700

    ### Home UI

--- app/src/main/java/com/example/moneytrack/MainActivity.kt
+++ app/src/main/java/com/example/moneytrack/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.moneytrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moneytrack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
