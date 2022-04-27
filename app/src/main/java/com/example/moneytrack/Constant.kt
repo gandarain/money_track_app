99d6bac8b7284b67f308f17a3f63f1f2c9a3aa25
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-04-27 21:27:38 +0700

    ### Add validation input

---
+++ app/src/main/java/com/example/moneytrack/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.moneytrack
+
+object Constant {
+    const val AMOUNT: String = "amount"
+    const val TITLE: String = "title"
+    const val DESCRIPTION: String = "description"
+    const val DATE: String = "date"
+}
--- app/src/main/java/com/example/moneytrack/CreateScreenActivity.kt
+++ app/src/main/java/com/example/moneytrack/CreateScreenActivity.kt
@@ -1,8 +1,12 @@
 package com.example.moneytrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.moneytrack.databinding.ActivityCreateScreenBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CreateScreenActivity : AppCompatActivity() {
     private var binding: ActivityCreateScreenBinding? = null
@@ -13,11 +17,12 @@ class CreateScreenActivity : AppCompatActivity() {
         setContentView(binding?.root)
 
         setupToolbar()
+
+        setupInput()
+
+        onSubmit()
     }
 
-    /**
-     * Setup the toolbar
-     */
     private fun setupToolbar() {
         // set the toolbar
         setSupportActionBar(binding?.toolBarCreate)
@@ -30,6 +35,48 @@ class CreateScreenActivity : AppCompatActivity() {
         // on press back
         binding?.toolBarCreate?.setNavigationOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun setupInput() {
+        if (intent.getStringExtra(Constant.AMOUNT) != null) {
+            binding?.etAmount?.setText(intent.getStringExtra(Constant.AMOUNT))
+        }
+
+        if (intent.getStringExtra(Constant.TITLE) != null) {
+            binding?.etTitle?.setText(intent.getStringExtra(Constant.TITLE))
+        }
+
+        if (intent.getStringExtra(Constant.DESCRIPTION) != null) {
+            binding?.etDescription?.setText(intent.getStringExtra(Constant.DESCRIPTION))
+        }
+    }
+
+    private fun generateDate(): String {
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        return sdf.format(dateTime)
+    }
+
+    private fun onSubmit() {
+        binding?.btnSubmit?.setOnClickListener {
+            val title = binding?.etTitle?.text
+            val description = binding?.etDescription?.text
+            val amount = binding?.etAmount?.text
+            val date = generateDate()
+
+            if (title.isNullOrEmpty() && description.isNullOrEmpty() && amount.isNullOrEmpty()) {
+                Toast.makeText(this, "Please fill all of the field!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Success submit the data!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(
+                    this@CreateScreenActivity,
+                    MainActivity::class.java
+                )
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
