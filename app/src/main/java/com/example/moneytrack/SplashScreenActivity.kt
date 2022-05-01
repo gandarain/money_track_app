352a507412eef4184d4b0cd7c42e33efdc78f530
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2022-05-01 14:50:46 +0700

    ### Change the Timer on Splash Screen

--- app/src/main/java/com/example/moneytrack/SplashScreenActivity.kt
+++ app/src/main/java/com/example/moneytrack/SplashScreenActivity.kt
@@ -19,6 +19,6 @@ class SplashScreenActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        }, 2000) // 2000 is the delayed time in milliseconds.
     }
 }
